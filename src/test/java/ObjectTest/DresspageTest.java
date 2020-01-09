ed3399f35b7a9d1a5f370f458a500a20e7e6c7be
YakeshPC <rakhesh.stp@gmail.com>
2020-01-09 12:19:18 +0530

    charlie

--- src/test/java/ObjectTest/DresspageTest.java
+++ src/test/java/ObjectTest/DresspageTest.java
@@ -14,7 +14,7 @@ public class DresspageTest {
 		HomePage hp;
 		Dresspage dp;
 		
-		public DresspageTest12() {
+		public DresspageTest12678() {
 			bp= new BasePage();
 			hp= new HomePage();
 			dp= new Dresspage();
